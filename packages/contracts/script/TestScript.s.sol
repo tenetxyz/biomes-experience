41f3ba1fc74dbb00e09538e2ace1084e40954a78
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-07-08 15:03:36 +0100

    build for drops contract

--- packages/contracts/script/TestScript.s.sol
+++ packages/contracts/script/TestScript.s.sol
@@ -11,6 +11,8 @@ import { VoxelCoord } from "@biomesaw/utils/src/Types.sol";
 import { Metadata } from "../src/codegen/tables/Metadata.sol";
 import { IExperience } from "../src/IExperience.sol";
 
+import { Build } from "@biomesaw/experience/src/utils/BuildUtils.sol";
+
 contract TestScript is Script {
   function run(address worldAddress) external {
     // Specify a store so that you can use tables directly in PostDeploy
@@ -26,7 +28,28 @@ contract TestScript is Script {
     address experienceAddress = Metadata.getExperienceAddress();
     console.logAddress(experienceAddress);
 
-    IExperience(experienceAddress).joinExperience{ value: 0 }();
+    uint8[] memory objectTypeIds = new uint8[](6);
+    objectTypeIds[0] = 42;
+    objectTypeIds[1] = 42;
+    objectTypeIds[2] = 42;
+    objectTypeIds[3] = 34;
+    objectTypeIds[4] = 42;
+    objectTypeIds[5] = 34;
+
+    VoxelCoord[] memory relativePositions = new VoxelCoord[](6);
+    relativePositions[0] = VoxelCoord(0, 0, 0);
+    relativePositions[1] = VoxelCoord(0, 0, 1);
+    relativePositions[2] = VoxelCoord(0, 0, 2);
+    relativePositions[3] = VoxelCoord(0, 1, 0);
+    relativePositions[4] = VoxelCoord(0, 1, 1);
+    relativePositions[5] = VoxelCoord(0, 1, 2);
+
+    IExperience(experienceAddress).setBuildForDrops(
+      "Logo",
+      Build({ objectTypeIds: objectTypeIds, relativePositions: relativePositions })
+    );
+    // IExperience(experienceAddress).matchBuild(VoxelCoord(303, 13, -251));
+    // IExperience(experienceAddress).dropItem(0x000000000000000000000000000000000000000000000000000000000000141e);
 
     vm.stopBroadcast();
   }
--- packages/contracts/src/IExperience.sol
+++ packages/contracts/src/IExperience.sol
@@ -4,8 +4,17 @@ pragma solidity >=0.8.24;
 import { ICustomUnregisterDelegation } from "@latticexyz/world/src/ICustomUnregisterDelegation.sol";
 import { IOptionalSystemHook } from "@latticexyz/world/src/IOptionalSystemHook.sol";
 
+import { VoxelCoord } from "@biomesaw/utils/src/Types.sol";
+import { Build } from "@biomesaw/experience/src/utils/BuildUtils.sol";
+
 interface IExperience is ICustomUnregisterDelegation, IOptionalSystemHook {
   function joinExperience() external payable;
 
   function getBiomeWorldAddress() external view returns (address);
+
+  function setBuildForDrops(string memory name, Build memory build) external;
+
+  function matchBuild(VoxelCoord memory baseWorldCoord) external;
+
+  function dropItem(bytes32 toolEntityId) external;
 }
