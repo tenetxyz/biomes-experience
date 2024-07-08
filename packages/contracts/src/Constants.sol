4af2e5cb3fde76e07afd14c0eff66bf314c22099
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-07-08 13:22:06 +0100

    deathmatch contract

--- packages/contracts/src/Constants.sol
+++ packages/contracts/src/Constants.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.24;
 
-bytes14 constant EXPERIENCE_NAMESPACE = bytes14("testexperience");
+bytes14 constant EXPERIENCE_NAMESPACE = bytes14("deathmatch");
+bytes32 constant DEATHMATCH_AREA_ID = bytes32(keccak256("deathmatch-area"));
