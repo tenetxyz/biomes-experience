70e5c47cacefe4eda9f21017c35b21e8ec8d6e5f
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-07-08 12:47:53 +0100

    race to the sky contract

--- packages/contracts/src/IExperience.sol
+++ packages/contracts/src/IExperience.sol
@@ -3,9 +3,12 @@ pragma solidity >=0.8.24;
 
 import { ICustomUnregisterDelegation } from "@latticexyz/world/src/ICustomUnregisterDelegation.sol";
 import { IOptionalSystemHook } from "@latticexyz/world/src/IOptionalSystemHook.sol";
+import { Area } from "@biomesaw/experience/src/utils/AreaUtils.sol";
 
 interface IExperience is ICustomUnregisterDelegation, IOptionalSystemHook {
   function joinExperience() external payable;
+
+  function setupGame(string memory name, Area memory area) external payable;
 
   function getBiomeWorldAddress() external view returns (address);
 }
