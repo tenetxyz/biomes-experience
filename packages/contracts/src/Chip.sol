a6e9c9b20e71f2038e87037c436685d500c42e52
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-11-09 18:26:09 +0700

    receive eth fallback

--- packages/contracts/src/Chip.sol
+++ packages/contracts/src/Chip.sol
@@ -116,4 +116,8 @@ contract Chip is IChip {
     bytes32[] memory toolEntityIds,
     bytes memory extraData
   ) public payable override onlyBiomeWorld returns (bool isAllowed) {}
+
+  receive() external payable {
+    // This function is executed when a contract receives plain Ether (without data)
+  }
 }
