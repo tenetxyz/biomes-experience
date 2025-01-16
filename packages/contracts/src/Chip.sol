8cdcc0f32ee0279ab634039ab78ea85d6bec6ffa
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-01-16 13:47:48 -0700

    update type

--- packages/contracts/src/Chip.sol
+++ packages/contracts/src/Chip.sol
@@ -125,15 +125,13 @@ contract Chip is IChip {
   function onChipHit(bytes32 callerEntityId, bytes32 targetEntityId) public override onlyBiomeWorld {}
 
   function onTransfer(
-    ChipOnTransferData memory transferContext,
-    bytes memory extraData
+    ChipOnTransferData memory transferContext
   ) public payable override onlyBiomeWorld returns (bool isAllowed) {
     return false;
   }
 
   function onPipeTransfer(
-    ChipOnPipeTransferData memory transferContext,
-    bytes memory extraData
+    ChipOnPipeTransferData memory transferContext
   ) public payable override onlyBiomeWorld returns (bool isAllowed) {
     return false;
   }
