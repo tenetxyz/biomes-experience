50fa9b3488a435eeef9de01e92ec16f6fdb3df9d
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-01-27 13:35:23 -0700

    item nft chest contract

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -5,13 +5,48 @@ export default defineWorld({
   deploy: {
     upgradeableWorldImplementation: true,
   },
-  namespace: "testchip",
+  namespace: "itemnftchest",
+  userTypes: {
+    ResourceId: { filePath: "@latticexyz/store/src/ResourceId.sol", type: "bytes32" },
+  },
   tables: {
     Metadata: {
       schema: {
         chipAddress: "address",
       },
       key: [],
+    },
+    ShopNFT: {
+      schema: {
+        chestEntityId: "bytes32",
+        nftNamespaceId: "ResourceId",
+      },
+      key: ["chestEntityId"],
+    },
+    OwnedNFTs: {
+      schema: {
+        owner: "address",
+        nfts: "address[]",
+      },
+      key: ["owner"],
+    },
+    NFTMetadata: {
+      schema: {
+        nftAddress: "address",
+        owner: "address",
+        objectTypeId: "uint8",
+        objectAmount: "uint256",
+        nftNextTokenId: "uint256",
+      },
+      key: ["nftAddress"],
+    },
+    MintedNFT: {
+      schema: {
+        player: "address",
+        nftAddress: "address",
+        minted: "bool",
+      },
+      key: ["player", "nftAddress"],
     },
   },
 });
